c8de0db1cfa8789b39148cb28bcf7cd4bd8da60b
weoz <weoz@ukr.net>
2016-10-11 20:16:32 +0300

    11. mysql.properties; created sql scripts(initDB, populateDB)

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,3 @@
+INSERT INTO  line(value_of_line, counter) VALUES
+  ('When you want to take a nap, the violin will wake you up!', 3),
+  ('You''re a child, Jake. Dress, don''t dress. Wash, don''t wash. I don''t care. You''re on your own.', 1);
