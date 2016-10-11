a7b801f8ba4c170de4726cf934d515d08b35b529
weoz <weoz@ukr.net>
2016-10-11 22:12:43 +0300

    12. added orm and configured Entity(Line)

--- src/main/java/com/bintime/model/BaseEntity.java
+++ src/main/java/com/bintime/model/BaseEntity.java
@@ -1,15 +1,21 @@
 package com.bintime.model;
 
+import javax.persistence.*;
+
 /**
  * Base class for all business entities
  *
  * @author Krukovskiy Arthur
  *
  */
+@MappedSuperclass
+@Access(AccessType.FIELD)
 public class BaseEntity {
     /**
      * Unique entity identifier
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected Integer id;
 
     public BaseEntity() {
--- src/main/java/com/bintime/model/Line.java
+++ src/main/java/com/bintime/model/Line.java
@@ -1,5 +1,9 @@
 package com.bintime.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Contains value of the line/string and the number of occurrences
  * of the string in all files that were transferred during one particular request
@@ -7,11 +11,19 @@ package com.bintime.model;
  * @author Krukovskiy Arthur
  *
  */
-public class Line {
+@Entity
+@Table(name = "line")
+public class Line extends BaseEntity{
 
+    @Column(name = "value_of_line")
     private String value;
 
+    @Column(name = "counter")
     private int count;
+
+    public Line() {
+        this.count = 1;
+    }
 
     public Line(String value) {
         this.value = value;
